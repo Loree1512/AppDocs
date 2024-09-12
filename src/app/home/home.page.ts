5bff949e5a6ba7dfca647c0c48e8d8df30b6cd4c
Loree1512 <loreto.bravo@outlook.com>
2024-09-12 09:56:08 -0300

    animacion penca

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AnimationController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,23 @@ import { Router } from '@angular/router';
 export class HomePage implements OnInit{
   username: string = '';
   
-  constructor(private router: Router) {}
+  constructor(private router: Router, private animationCtrl: AnimationController) {}
+
+  async animateButton(target: EventTarget | null) {
+    if (!(target instanceof HTMLElement)) return;
+  
+    const button = target as HTMLElement;
+    const animation = this.animationCtrl.create()
+      .addElement(button)
+      .duration(400)
+      .keyframes([
+        { offset: 0, transform: 'scale(1)' },
+        { offset: 0.5, transform: 'scale(2)' },
+        { offset: 1, transform: 'scale(1)' }
+      ]);
+  
+    await animation.play();
+  }
 
   ngOnInit() {
     this.getUser();
